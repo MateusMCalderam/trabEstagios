ecb14da27fea164a8c5a2236a97f57bdc3f015d7
Maria Eduarda Weber <mariaeduardaweber509@gmail.com>
2024-11-22 16:16:04 -0300

    Update EstudanteModel.php

--- models/EstudanteModel.php
+++ models/EstudanteModel.php
@@ -14,7 +14,7 @@ final class EstudanteModel extends Model {
         $arrayDados = [];
 
         foreach($data as $row) {
-            $vo = new EstudanteVO($row["id"], $row["nome"], $row["matricula"], $row["curso"]);
+            $vo = new EstudanteVO($row["id"], $row["nome"], $row["matricula"], $row["curso"], $row["cpf"], $row["rg"], $row["cidade"], $row["telefone"], $row["email"]);
             array_push($arrayDados, $vo);
         }
 
@@ -27,27 +27,24 @@ final class EstudanteModel extends Model {
         $binds = [":id" => $vo->getId()];
         $data = $db->select($query, $binds);
 
-        return new EstudanteVO($data[0]["id"], $data[0]["nome"], $data[0]["matricula"], $data[0]["curso"]);
+        return new EstudanteVO($data[0]["id"], $data[0]["nome"], $data[0]["matricula"], $data[0]["curso"], $data[0]["cpf"], $data[0]["rg"], $data[0]["cidade"], $data[0]["telefone"], $data[0]["email"]);
     }
 
     public function insert($vo) {
         $db = new Database();
 
-        if(empty($vo->getCurso())){
-            $query = "INSERT INTO estudante (nome, matricula ) VALUES (:nome, :matricula)";
-            $binds = [
-                ":nome" => $vo->getNome(),
-                ":matricula" => $vo->getMatricula(),
-            ];
-        }else{
-            $query = "INSERT INTO estudante (nome, matricula, curso) VALUES (:nome, :matricula, :curso)";
-            $binds = [
-                ":nome" => $vo->getNome(),
-                ":matricula" => $vo->getMatricula(),
-                ":curso" => $vo->getCurso()
-            ];
-        }
-        
+        $query = "INSERT INTO estudante (nome, matricula, curso, cpf, rg, cidade, telefone, email) VALUES (:nome, :matricula, :curso, :cpf, :rg, :cidade, :telefone, :email)";
+        $binds = [
+            ":nome" => $vo->getNome(),
+            ":matricula" => $vo->getMatricula(),
+            ":curso" => $vo->getCurso(),
+            ":cpf" => $vo->getCurso(),
+            ":rg" => $vo->getRg(),
+            ":cidade" => $vo->getCidade(),
+            ":telefone" => $vo->getTelefone(),
+            ":email" => $vo->getEmail(),
+        ];
+    
 
         return $db->execute($query, $binds);
     }
@@ -55,22 +52,19 @@ final class EstudanteModel extends Model {
     public function update($vo) {
         $db = new Database();
 
-        if(empty($vo->getCurso())){
-            $query = "UPDATE estudante SET nome = :nome, matricula = :matricula WHERE id = :id";
-            $binds = [
-                ":nome" => $vo->getNome(),
-                ":matricula" => $vo->getMatricula(),
-                ":id" => $vo->getId()
-            ];
-        }else{
-            $query = "UPDATE estudante SET nome = :nome, matricula = :matricula, curso = :curso WHERE id = :id";
-            $binds = [
-                ":nome" => $vo->getNome(),
-                ":matricula" => $vo->getMatricula(),
-                ":id" => $vo->getId(),
-                ":curso" => $vo->getCurso()
-            ];
-        }
+        $query = "UPDATE estudante SET nome = :nome, matricula = :matricula, curso = :curso, cpf = :cpf, rg = :rg, 
+        cidade = :cidade, telefone = :telefone, email = :email WHERE id = :id";
+        $binds = [
+            ":nome" => $vo->getNome(),
+            ":matricula" => $vo->getMatricula(),
+            ":curso" => $vo->getCurso(),
+            ":cpf" => $vo->getCurso(),
+            ":rg" => $vo->getRg(),
+            ":cidade" => $vo->getCidade(),
+            ":telefone" => $vo->getTelefone(),
+            ":email" => $vo->getEmail(),
+            ":id" => $vo->getId()
+        ];
         
 
         return $db->execute($query, $binds);
