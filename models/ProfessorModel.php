1e86d4e2ae79688e9ab627eda5cb30c04934e895
Maria Eduarda Weber <mariaeduardaweber509@gmail.com>
2024-11-27 10:58:19 -0300

    Create ProfessorModel

--- models/ProfessorModel.php
+++ models/ProfessorModel.php
@@ -2,19 +2,19 @@
 
 namespace Model;
 
-use Model\VO\CoordenadorVO;
+use Model\VO\ProfessorVO;
 
-final class CoordenadorModel extends Model {
+final class ProfessorModel extends Model {
 
     public function selectAll($vo) {
         $db = new Database();
-        $query = "SELECT * FROM coordenador";
+        $query = "SELECT * FROM professor";
         $data = $db->select($query);
 
         $arrayDados = [];
 
         foreach($data as $row) {
-            $vo = new CoordenadorVO($row["id"], $row["nome"], $row["email"]);
+            $vo = new ProfessorVO($row["id"], $row["nome"], $row["email"], $row["siape"]);
             array_push($arrayDados, $vo);
         }
 
@@ -23,20 +23,21 @@ final class CoordenadorModel extends Model {
 
     public function selectOne($vo) {
         $db = new Database();
-        $query = "SELECT * FROM coordenador WHERE id = :id";
+        $query = "SELECT * FROM professor WHERE id = :id";
         $binds = [":id" => $vo->getId()];
         $data = $db->select($query, $binds);
 
-        return new CoordenadorVO($data[0]["id"], $data[0]["nome"], $data[0]["email"]);
+        return new ProfessorVO($data[0]["id"], $data[0]["nome"], $data[0]["email"], $data[0]["siape"]);
     }
 
     public function insert($vo) {
         $db = new Database();
 
-        $query = "INSERT INTO coordenador (nome, email) VALUES (:nome, :email)";
+        $query = "INSERT INTO professor (nome, email, siape) VALUES (:nome, :email, :siape)";
         $binds = [
             ":nome" => $vo->getNome(),
             ":email" => $vo->getEmail(),
+            ":siape" => $vo->getSiape()
         ];
     
 
@@ -46,10 +47,11 @@ final class CoordenadorModel extends Model {
     public function update($vo) {
         $db = new Database();
 
-        $query = "UPDATE coordenador SET nome = :nome, email = :email WHERE id = :id";
+        $query = "UPDATE professor SET nome = :nome, email = :email WHERE id = :id";
         $binds = [
             ":nome" => $vo->getNome(),
             ":email" => $vo->getEmail(),
+            ":siape" => $vo->getSiape(),
             ":id" => $vo->getId()
         ];
         
@@ -59,7 +61,7 @@ final class CoordenadorModel extends Model {
 
     public function delete($vo) {
         $db = new Database();
-        $query = "DELETE FROM coordenador WHERE id = :id";
+        $query = "DELETE FROM professor WHERE id = :id";
         $binds = [":id" => $vo->getId()];
 
         return $db->execute($query, $binds);
