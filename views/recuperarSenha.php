b93bc4369f8543e84b314f464b4f4a53cdce665e
Dudiiss32 <eduardabellow@gmail.com>
2024-12-09 21:35:02 -0300

    update css views

--- views/recuperarSenha.php
+++ views/recuperarSenha.php
@@ -3,24 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/form.css">
     <base href="./trabEstagios">
     <title>Recuperação de Senha</title>
 </head>
 <body>
-    <h2>Recuperação de Senha</h2>
-    <?php
-    if (isset($erro)) {
-      echo $erro;
-    }
-    ?>
-    <br>
-    <br>
-    <form action="./enviarRecuperacao" method="POST">
-        <label for="email">E-mail</label>
-        <input type="email" id="email" name="email" required placeholder="Digite seu e-mail">
-        <br>
-        <button type="submit">Enviar link</button>
-    </form>
-    <p><a href="./login">Voltar ao login</a></p>
+    <div class="body">
+        <h2>Recuperação de Senha</h2>
+        <?php
+        if (isset($erro)) {
+        echo $erro;
+        }
+        ?>
+        <form action="./enviarRecuperacao" method="POST">
+            <label for="email">E-mail</label>
+            <input type="email" id="email" name="email" required placeholder="Digite seu e-mail">
+            <br>
+            <button type="submit">Enviar link</button>
+        </form>
+        <p><a href="./login">Voltar ao login</a></p>
+    </div>
+    
 </body>
 </html>
--- views/resetSenha.php
+++ views/resetSenha.php
@@ -3,32 +3,26 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/form.css">
   <base href="./trabEstagios">
   <title>Redefinir Senha</title>
 </head>
 <body>
-  <form action="./processarNovaSenha" method="POST">
-    <h2>Redefinir Senha</h2>
-    <?php
-    if (isset($erro)) {
-      echo $erro;
-    }
-    ?>
-    <br>
-    <br>
-    <input type="hidden" name="token" value="<?php echo isset($_GET['token']) ? $_GET['token'] : $token; ?>">
-
-    <label for="nova_senha">Nova Senha:</label>
-    <input type="password" id="nova_senha" name="nova_senha" required>
-
-    <br><br>
-
-    <label for="confirmar_nova_senha">Confirmar Nova Senha:</label>
-    <input type="password" id="confirmar_nova_senha" name="confirmar_nova_senha" required>
-
-    <br><br>
-
-    <button type="submit">Redefinir Senha</button>
-  </form>
+  <div class="body">
+    <form action="./processarNovaSenha" method="POST">
+      <h2>Redefinir Senha</h2>
+      <?php
+      if (isset($erro)) {
+        echo $erro;
+      }
+      ?>
+      <input type="hidden" name="token" value="<?php echo isset($_GET['token']) ? $_GET['token'] : $token; ?>">
+      <label for="nova_senha">Nova Senha:</label>
+      <input type="password" id="nova_senha" name="nova_senha" required>
+      <label for="confirmar_nova_senha">Confirmar Nova Senha:</label>
+      <input type="password" id="confirmar_nova_senha" name="confirmar_nova_senha" required>
+      <button type="submit">Redefinir Senha</button>
+    </form>
+  </div>
 </body>
 </html>
